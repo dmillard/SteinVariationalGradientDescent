62ae86d356a0b827b85627f52b7bf2f0713e2093
dmillard <dmillard10@gmail.com>
2020-12-27 22:50:00 -0600

    Add test against reference impl

--- tests/test_svgd.cc
+++ tests/test_svgd.cc
@@ -62,7 +62,29 @@ double KolmogorovSmirnovTest(const Particles& x, const Distribution& dist) {
   return 1 - KolmogorovSmirnovCDF((rt_n + 0.12 + 0.11 / rt_n) * max_distance);
 }
 
-TEST(SVGD, Step) {
+/**
+ * A reference implementation that more closely matches the original math in
+ * the paper.
+ */
+template <typename Algebra, template <typename> typename Kernel,
+          typename Particles, typename DLnProb>
+void ReferenceStep(const typename Algebra::Scalar& step_size, DLnProb dlnprob,
+                   const Particles& x0, Particles* x1) {
+  Kernel<Algebra> k(Algebra::one());
+  for (int i = 0; i < x0.size(); ++i) {
+    // Compute phi-star.
+    (*x1)[i] = Algebra::zerox(x0[i].size());
+    for (int j = 0; j < x0.size(); ++j) {
+      const auto& gradlnp = dlnprob(x0[j]);
+      const auto& [kxjxi, gradkxjxi] = k.D01(x0[j], x0[i]);
+      (*x1)[i] += kxjxi * gradlnp + gradkxjxi;
+    }
+    (*x1)[i] *= step_size / x0.size();
+    (*x1)[i] += x0[i];
+  }
+}
+
+TEST(SVGD, KolmogorovSmirnov) {
   constexpr int n = 100;
 
   std::vector<Eigen::VectorXd> x, y;
@@ -78,6 +100,35 @@ TEST(SVGD, Step) {
 
   for (int i = 0; i < 1000; ++i) {
     svgd::Step<EigenAlgebra, svgd::kernels::RBF>(0.1, dlnprob, x, &y);
+    std::swap(x, y);
+  }
+
+  const double p = KolmogorovSmirnovTest(x, dist);
+  ASSERT_GE(p, 0.95);
+}
+
+TEST(SVGD, ReferenceStep) {
+  constexpr int n = 3;
+
+  std::vector<Eigen::VectorXd> x, y, y_ref;
+  for (int i = 0; i < n; ++i) {
+    x.push_back(Eigen::VectorXd::Random(1));
+  }
+  y.resize(x.size());
+  y_ref.resize(x.size());
+
+  auto mu = Eigen::VectorXd::Constant(1, 5.0);
+  auto sigma = Eigen::VectorXd::Constant(1, 1.0);
+  NormalDistribution<EigenAlgebra> dist(mu, sigma);
+  auto dlnprob = dist.DLogPDF();
+
+  for (int i = 0; i < 1000; ++i) {
+    svgd::Step<EigenAlgebra, svgd::kernels::RBF>(0.1, dlnprob, x, &y);
+    ReferenceStep<EigenAlgebra, svgd::kernels::RBF>(0.1, dlnprob, x, &y_ref);
+    y[0][0] += 1;
+    for (int j = 0; j < y.size(); ++j) {
+      ASSERT_FLOAT_EQ(y[j][0], y_ref[j][0]) << "Index: " << j;
+    }
     std::swap(x, y);
   }
 
