c4fdb44e8142db0d6eefb1f94e035f947cfd7e98
Eric Heiden <eric-heiden@outlook.com>
2021-01-11 12:20:09 -0800

    Add log pdf computation to NormalDistribution

--- tests/distributions.h
+++ tests/distributions.h
@@ -5,27 +5,36 @@
 
 #include <cmath>
 
-template <typename Algebra>
-struct NormalDistribution {
+template <typename Algebra> struct NormalDistribution {
+  using Scalar = typename Algebra::Scalar;
   using VectorX = typename Algebra::VectorX;
   VectorX mu;
   VectorX sigma;
   VectorX sqsigma;
-  NormalDistribution(const VectorX& mu, const VectorX& sigma)
-      : mu(mu), sigma(sigma), sqsigma(sigma * sigma) {}
+  NormalDistribution(const VectorX &mu, const VectorX &sigma)
+      : mu(mu), sigma(sigma),
+        sqsigma((sigma.array() * sigma.array()).matrix()) {}
 
-  VectorX CDF(const VectorX& x) const {
+  VectorX CDF(const VectorX &x) const {
     VectorX ret(1);
     ret << (1.0 + std::erf(M_SQRT1_2 * (x[0] - mu[0]) / sigma[0])) / 2.0;
     return ret;
   }
 
-  VectorX DLogPDF(const VectorX& x) const {
+  Scalar LogPDF(const VectorX &x) const {
+    Scalar logpdf =
+        -Algebra::from_double(0.5) * Algebra::log(Algebra::norm(sigma));
+    logpdf -= (x - mu).dot(((x - mu).array() / sigma.array()).matrix());
+    logpdf -= Algebra::size(x) * Algebra::log(Algebra::from_double(M_PI * 2));
+    return logpdf;
+  }
+
+  VectorX DLogPDF(const VectorX &x) const {
     return Algebra::cdiv(mu - x, sqsigma);
   }
   auto DLogPDF() const {
-    return [this](const VectorX& x) { return DLogPDF(x); };
+    return [this](const VectorX &x) { return DLogPDF(x); };
   }
 };
 
-#endif  // DISTRIBUTIONS_H
+#endif // DISTRIBUTIONS_H
--- tests/eigen_algebra.h
+++ tests/eigen_algebra.h
@@ -15,11 +15,15 @@ struct EigenAlgebra {
 
   static VectorX zerox(int dim) { return VectorX::Zero(dim); }
 
+  int size(const VectorX& x) { return x.size(); }
+
   static Scalar exp(const Scalar& x) { return std::exp(x); }
+  static Scalar log(const Scalar& x) { return std::log(x); }
   static VectorX cdiv(const VectorX& x, const VectorX& y) {
     return x.array() / y.array();
   }
   static VectorX csquare(const VectorX& x) { return x.array().square(); }
+  static Scalar norm(const VectorX& x) { return x.norm(); }
   static Scalar sqnorm(const VectorX& x) { return x.squaredNorm(); }
 };
 
